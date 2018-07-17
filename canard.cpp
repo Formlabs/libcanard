5166cb26d10f25c1cda6aebae0f330e0fd775dac
Manuel Odendahl <wesen@ruinwesen.com>
2018-07-17 14:21:03 -0400

    :tractor: Move files and code around

--- canard.cpp
+++ canard.cpp
@@ -1,101 +1,27 @@
 #include <canard.h>
 #include <cstdint>
 #include <drivers/stm32/canard_stm32.h>
-
-// from: https://stackoverflow.com/questions/7038797/automatically-pick-a-variable-type-big-enough-to-hold-a-specified-number
-
-#include <limits>
-#include <type_traits>
-
-template<class T, class U =
-typename std::conditional<std::is_signed<T>::value,
-    std::intmax_t,
-    std::uintmax_t
->::type>
-constexpr bool is_in_range(U x) {
-    return (x >= std::numeric_limits<T>::min())
-           && (x <= std::numeric_limits<T>::max());
-}
-
-template<std::intmax_t x>
-using int_fit_type =
-typename std::conditional<is_in_range<std::int8_t>(x),
-    std::int8_t,
-    typename std::conditional<is_in_range<std::int16_t>(x),
-        std::int16_t,
-        typename std::conditional<is_in_range<std::int32_t>(x),
-            std::int32_t,
-            typename std::enable_if<is_in_range<std::int64_t>(x), std::int64_t>::type
-        >::type
-    >::type
->::type;
-
-template<std::uintmax_t x>
-using uint_fit_type =
-typename std::conditional<is_in_range<std::uint8_t>(x),
-    std::uint8_t,
-    typename std::conditional<is_in_range<std::uint16_t>(x),
-        std::uint16_t,
-        typename std::conditional<is_in_range<std::uint32_t>(x),
-            std::uint32_t,
-            typename std::enable_if<is_in_range<std::uint64_t>(x), std::uint64_t>::type
-        >::type
-    >::type
->::type;
-
-template<
-    typename T,
-    std::uintmax_t SIZE>
-
-class RingBuffer {
-public:
-    RingBuffer() = default;
-
-    typedef uint_fit_type<SIZE> msize_t;
-
-    static inline msize_t inc(msize_t v) {
-        return (v + 1) % SIZE;
-    }
-
-    // No boundary checking, call IsFull() first
-    inline T &get_next_write_entry() {
-        return data[writeIdx];
-    }
-
-    inline T &get_next_read_entry() {
-        return data[readIdx];
-    }
-
-    inline bool is_full() const {
-        return inc(writeIdx) == readIdx;
-    }
-
-    inline bool is_empty() const {
-        return readIdx == writeIdx;
-    }
-
-    inline void advance_write() {
-        writeIdx = inc(writeIdx);
-    }
-
-    inline void advance_read() {
-        readIdx = inc(readIdx);
-    }
-
-private:
-    T data[SIZE];
-
-    volatile msize_t readIdx = 0;
-    volatile msize_t writeIdx = 0;
-};
-
-#define USED __attribute__((used))
+#include <helpers/RingBuffer.h>
+#include <helpers/helpers.h>
 
 static RingBuffer<CanardCANFrame, CAN_RX_RB_SIZE> CAN_RX_RB;
 static RingBuffer<CanardCANFrame, CAN_TX_RB_SIZE> CAN_TX_RB;
 
+int getRXSize() {
+    return CAN_RX_RB.size();
+}
+
+int getTXSize() {
+    return CAN_TX_RB.size();
+}
+
+
 extern "C" {
 extern CanardInstance canard;
+extern volatile int allTimeMaxRxSize;
+extern volatile int allTimeMaxTxSize;
+extern volatile int maxRxSize;
+extern volatile int maxTxSize;
 
 #if defined(STM32F0) || defined(STM32F3)
 void USED CAN_IT_Callback() {
@@ -107,6 +33,13 @@ void USED CAN_IT_Callback() {
         canard_errors.rx_errors++;
     }
 
+    volatile int rxSize = getRXSize();
+    if (rxSize > maxRxSize) {
+        maxRxSize = rxSize;
+    }
+    if (rxSize > allTimeMaxRxSize) {
+        allTimeMaxRxSize = rxSize;
+    }
     canardSTM32Receive_IT();
 }
 #endif
@@ -144,6 +77,15 @@ void processTxQueue(void) {
         } else {
             CAN_TX_RB.advance_read();
         }
+
+        volatile int txSize = getTXSize();
+        if (txSize > maxTxSize) {
+            maxTxSize = txSize;
+        }
+        if (txSize > allTimeMaxTxSize) {
+            allTimeMaxTxSize = txSize;
+        }
+
     } else {
         canTXTransmitting = false;
     }
